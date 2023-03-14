18625bbc2c4076fdd06517af807c6bc53e7574ac
YayisQuiroz <126173181+YayisQuiroz@users.noreply.github.com>
2023-03-13 22:37:10 -0600

    Add files via upload

---
+++ polimorismo/polimorismo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polimorismo
+{
+
+    public interface IAnimal
+    {
+        void HacerSonido();
+    }
+
+    public class Perro : IAnimal
+    {
+        public void HacerSonido()
+        {
+            Console.WriteLine("Guau guau");
+        }
+    }
+
+    public class Gato : IAnimal
+    {
+        public void HacerSonido()
+        {
+            Console.WriteLine("Miau miau");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<IAnimal> animales = new List<IAnimal>();
+
+            animales.Add(new Perro());
+            animales.Add(new Gato());
+
+  
+            foreach (IAnimal animal in animales)
+            {
+                animal.HacerSonido();
+            }
+            Console.ReadKey();
+        }
+    }
+ 
+}
