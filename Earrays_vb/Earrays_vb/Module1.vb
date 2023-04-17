d86162799cc73dbc4a1126e72f3cd2877d16507c
YayisQuiroz <126173181+YayisQuiroz@users.noreply.github.com>
2023-04-16 21:42:40 -0600

    Add files via upload

---
+++ Earrays_vb/Earrays_vb/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+    Sub Main()
+        ' Crear un arreglo de nombres'
+        Dim nombres() As String = {"Juan", "María", "Pedro", "Luisa"}
+
+        ' Recorrer el arreglo e imprimir cada nombre'
+        Console.WriteLine("Los nombres en el arreglo son:")
+        For i As Integer = 0 To nombres.Length - 1
+            Console.WriteLine(nombres(i))
+        Next
+
+        ' Esperar a que el usuario presione una tecla para salir'
+        Console.ReadKey()
+    End Sub
+End Module
